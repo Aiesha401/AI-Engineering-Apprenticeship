1e379daedea693e26490acb5d2f99a32ead25b26
Aiesha Shaik <aieshashaik4023@gmail.com>
2026-07-15 21:46:13 +0530

    Implement System Prompts

---
+++ commerceops-ai/experiments/day5_system_prompt.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key=os.getenv("NVIDIA_API_KEY")
+)
+
+messages = [
+    {
+        "role" : "system",
+        "content" : "You are a friendly Python mentor.\
+                    Always explain programming concepts in simple language.\
+                    Give a short example whenever possible.\
+                    Keep answers concise."
+    }
+]
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        print("Goodbye!")
+        break
+    messages.append({
+        "role": "user",
+        "content": user_input
+    })
+    response = client.chat.completions.create(
+        model="meta/llama-3.1-8b-instruct",
+        messages=messages,
+        temperature=0.2,
+        max_tokens=50
+    )
+    ai_response = response.choices[0].message.content
+    print("AI:", ai_response)
+    messages.append(
+        {
+            "role": "assistant",
+            "content": ai_response
+        }
+    )
